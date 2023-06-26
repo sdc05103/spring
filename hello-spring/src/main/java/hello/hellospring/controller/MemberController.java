d893f0aa2323d931c45afece3e72bdd893b44abd
sdc05103 <heeni1004@gmail.com>
2023-06-26 22:08:40 +0900

    final commit

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -18,12 +18,12 @@ public class MemberController{
         this.memberService = memberService;
     }
 
-    @GetMapping("/members/new")
+    @GetMapping("/join")
     public String createForm(){
         return "members/createMemberForm";
     }
 
-    @PostMapping("/members/new")
+    @PostMapping("/join")
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
@@ -33,10 +33,10 @@ public class MemberController{
         if (!member.getPwd().equals(member.getPwdConfirm())) {
             // 비밀번호와 비밀번호 확인이 일치하지 않는 경우 처리
             // 예를 들어, 비밀번호 오류 메시지를 보여주고 회원가입 폼으로 다시 이동할 수 있습니다.
-            return "redirect:/members/new"; //
+            return "redirect:/join"; //
         } else {
             memberService.join(member);
-            return "redirect:/";
+            return "redirect:/home";
         }
     }
     @GetMapping("/members")
